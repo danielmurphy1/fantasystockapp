3509063ba6777de1dcd8e22c47eb04c97f37a30c
Dan <daniel.murphy.1@gmail.com>
2021-02-12 16:23:56 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 3000;
+const app = express();
+const axios = require("axios");
+require("dotenv").config();
+
+app.use("/", express.static(path.join(__dirname, "client/build")));
+
+
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}.`);
+});

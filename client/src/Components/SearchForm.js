e12cbb26ff8df76485d652ba994010094fb32bc9
Dan <daniel.murphy.1@gmail.com>
2021-03-03 21:02:30 -0500

    created and styled Search Result Card

--- client/src/Components/SearchForm.js
+++ client/src/Components/SearchForm.js
@@ -3,7 +3,7 @@ import { Form, Button } from 'react-bootstrap';
 
 function SearchForm(props) {
     return(
-        <Form inline className="justify-content-around" onSubmit={props.handleStockSearch}>
+        <Form inline className="justify-content-around mb-3" onSubmit={props.handleStockSearch}>
             <Form.Group>
                 <Form.Label htmlFor="stockSymbolSearch" srOnly>Stock Symbol</Form.Label>
                 <Form.Control type="text" id="stockSymbolSearch" placeholder="Enter Stock Symbol" className="mr-1" />
--- client/src/Components/SearchResultCard.js
+++ client/src/Components/SearchResultCard.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
-import puppy from '../images/place-holder-line-graph.jpg';
+import { Card, Button, Container } from 'react-bootstrap';
+import '../Styles/SearchResultCard.css';
 
 
 function SearchResultCard(){
     return(
         <Card>
-            {/* <Card.Img variant="top" src={puppy}/> */}
-            <Card.Body>
-                <Card.Title>Title</Card.Title>
+            <Card.Header as="h3" className="text-left d-flex justify-content-between">Name of Stock
+                {/* <Container className=""> */}
+                <div>
+                    <Button className="mx-3">Buy</Button>
+                    <Button>Sell</Button>
+                {/* </Container> */}
+                </div>
+            </Card.Header>
+            <Card.Body className="d-flex justify-content-around">
+                <div>
+                    <h4>Current Shares</h4>
+                    <p>10</p>
+                </div>
+                <div>
+                    <h4>Current Price</h4>
+                    <p>$320.50</p>
+                </div>
+                <div>
+                    <h4>Precentage Change</h4>
+                    <p>+0.52%</p>
+                </div>
             </Card.Body>
         </Card>
     )

eb19230520ccc3e47caad560a8a25e1fb661d845
Dan <daniel.murphy.1@gmail.com>
2021-05-20 22:19:07 -0400

    added logout button functionality

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -8,6 +8,10 @@ function Header() {
 
     const isLoggedIn = authCtx.isLoggedIn;
 
+    function logoutHandler(){
+        authCtx.logout();
+    }
+
     return(
         <Navbar collapseOnSelect expand="lg" bg="light" variant="light" sticky="top">
             <Navbar.Brand href="#home"></Navbar.Brand>
@@ -17,7 +21,7 @@ function Header() {
                     <Nav.Link href="/">Home</Nav.Link>
                     {isLoggedIn && (<Nav.Link href="/trade">Trade</Nav.Link>)}
                     {isLoggedIn && (<Nav.Link href="/portfolio">Portfolio</Nav.Link>)}
-                    {isLoggedIn && (<Button>Logout</Button>)}
+                    {isLoggedIn && (<Button onClick={logoutHandler}>Logout</Button>)}
                 </Nav>
             </Navbar.Collapse>
         </Navbar>

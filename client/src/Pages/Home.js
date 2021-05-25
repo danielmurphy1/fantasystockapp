7c02f74298f9c38a023cdb86d305fe2fed85c119
Dan <daniel.murphy.1@gmail.com>
2021-05-24 22:20:51 -0400

    removed no longer needed code on home after switching to useRef

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -5,59 +5,10 @@ import LogInForm from '../Components/LogInForm';
 import SignUpForm from '../Components/SignUpForm';
 import AuthContext from '../store/auth-context';
 
-// function FormSwitch() { //this is one fix for the single character entry input bug
-//     const [ isRegistered, setIsRegistered ] = useState(true);
-//     const [ isLoggedIn, setIsLoggedIn ] = useState(false);
-//     const [ userName, setUserName] = useState('');
-//     const [ password, setPassword ] = useState('');
-
-//     if (isRegistered) {
-//         return <LogInForm 
-//                     isRegistered={toggleIsRegistered} 
-//                     handleUserNameChange={handleUserNameChange} 
-//                     handlePasswordChange={handlePasswordChange}
-//                     handleLogin={handleLogin}
-//                     userName={userName}
-//                     password={password}
-//                     />
-//     } else {
-//         return <SignUpForm isRegistered={toggleIsRegistered}/>
-//     };
-
-//     function toggleIsRegistered(event) {
-//         event.preventDefault();
-//         setIsRegistered(prevState => !prevState);
-//         console.log(isRegistered)
-//     };
-
-//     function handleLogin(event){
-//         event.preventDefault();
-//         if(userName && password.length > 0 ){
-//             axios.post('/api/login', {
-//                 username: 'admin', 
-//                 password: 'admin'
-//             });
-//             setIsLoggedIn(true);
-//         }
-//     };
-
-//     function handleUserNameChange(event){
-//         // const { name, value } = event.target;
-//         setUserName(event.target.value)
-//     };
-
-//     function handlePasswordChange(event){
-//         // const { name, value } = event.target;
-//         setPassword(event.target.value)
-//     };
-// };
-
 function Home() {
     const [ isRegistered, setIsRegistered ] = useState(true);
     const [ isLoggedIn, setIsLoggedIn ] = useState(false);
     const [ successfulReg, setSuccessfulReg ] = useState(false);
-    // const [ userName, setUserName] = useState('');
-    // const [ password, setPassword ] = useState('');
     const userNameInputRef = useRef();
     const passwordInputRef = useRef();
 
@@ -91,9 +42,6 @@ function Home() {
         if (isRegistered) {
             return <LogInForm 
                         isRegistered={toggleIsRegistered} 
-                        // handleUserNameChange={handleUserNameChange} 
-                        // handlePasswordChange={handlePasswordChange}
-                        // handleInputChange={handleInputChange}
                         handleLogin={handleLogin}
                         userNameInputRef={userNameInputRef}
                         passwordInputRef={passwordInputRef}
@@ -110,19 +58,7 @@ function Home() {
 
     function handleLogin(event){
         event.preventDefault();
-        // if(userName && password){
-        //     axios.post('/api/login', {
-        //         username: userName, 
-        //         password: password
-        //     })
-        //     .then(res => {
-        //         console.log(res);
-        //         setIsLoggedIn(true);
-        //     })
-        //     .catch(error => console.log(error.body));
-        //     setUserName('');
-        //     setPassword('');
-        // }
+
         const enteredUserName = userNameInputRef.current.value;
         const enteredPassword = passwordInputRef.current.value;
         if(enteredUserName && enteredPassword){
@@ -140,8 +76,6 @@ function Home() {
                 console.log(error)
                 alert(error)
             });
-            // setUserName('');
-            // setPassword('');
         }
     };
 
@@ -165,27 +99,6 @@ function Home() {
             }
         })
     };
-
-    // function handleUserNameChange(event){
-    //     setUserName(event.target.value)
-    // };
-
-    // function handlePasswordChange(event){
-    //     setPassword(event.target.value)
-    // };
-
-    // function handleInputChange(event){
-    //     if (event.target.name === 'userName'){
-    //         setUserName(event.target.value)
-    //     } else if (event.target.name === 'password'){
-    //         setPassword(event.target.value)
-    //     }
-    // };
-
-
-    // useEffect(() => {
-    //     handleLogin();
-    // }, {})
 
     return (
         <Container>

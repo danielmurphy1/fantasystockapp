411e5754a72ff56a908c694e3af2cce05dc8b032
Dan <daniel.murphy.1@gmail.com>
2021-06-17 21:32:32 -0400

    started chart template

---
+++ client/src/Components/ChartTemplate.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Chart } from 'react-google-charts';
+
+function ChartTemplate(props){
+    return(
+        <div>
+            <Chart
+                width={'100%'}
+                height={'500px'}
+                chartType={props.type}
+                loader={<div>Loading Chart</div>}
+                data={props.data}
+                options={{
+                    title: props.title,
+                    pointSize: 10,
+                    colors: ['#15A0C8'],
+                    series: {
+                        0: { 
+                            pointShape: 'circle',
+                            curveType: 'function'
+                        }
+                    },
+                    chartArea: { width: props.chartAreaWidth },
+                    hAxis: { title: props.hAxisTitle },
+                    vAxis: { title: props.vAxisTitle }
+                }}
+                rootProps={{ 'data-testid': '1' }}
+            />
+        </div>
+    )
+};
+
+export default ChartTemplate;

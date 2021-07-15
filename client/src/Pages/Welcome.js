858dd9a35b3478e97254037022e20d3fb71c0a4f
Dan <daniel.murphy.1@gmail.com>
2021-07-14 22:36:14 -0400

    added text to welcome page for logged in users

--- client/src/Pages/Welcome.js
+++ client/src/Pages/Welcome.js
@@ -1,45 +1,25 @@
 import React, { useContext } from 'react';
-import AuthContext from '../store/auth-context';
 import UserContext from '../store/user-context';
-import axios from 'axios';
-
 import { Container } from 'react-bootstrap';
 
 function Welcome() {
 
-    const authCtx = useContext(AuthContext);
     const userCtx = useContext(UserContext);
 
-    function getProtected(){
-        axios.get('/api/protected', {
-            headers:{
-                "Content-type": "application/json", 
-                "Accept": "application/json", 
-                "Authorization": "Bearer" + " " + authCtx.token
-            }
-        })
-        .then(res => console.log(res))
-        .catch(err => {
-            alert(err)
-            console.log(err.message)})
-    }
     return (
         <Container>
             <Container className="App welcome-container">
                 <h1>Welcome {userCtx.userName} to Fantasy Stock Trader</h1>
                 <p>
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                    eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-                    ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                    aliquip ex ea commodo consequat. Duis aute irure dolor in
-                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                    pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum."
+                    Head to the Trade page to search for stocks and to buy/sell shares. There you can also see information about the current stocks you hold
+                    and how much money is in your account.  
+                </p>
+                <p>
+                    The Portfolio page will show you graphic representations of your holdings. There you will see the percentage that each of your holdings 
+                    contributes to your portfolio as well as a stock to stock comparison of the number of shares held. There you will also
+                    see the total of all your assests combined from your cash account and the value of your stock shares. 
                 </p>
             </Container>
-            <h2>Login Successful</h2>
-            <button onClick={getProtected}>Protected</button>
-
         </Container>
         
     );

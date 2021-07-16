83124ecc9fa17db6650da9dcd51fe742c0727b2f
Dan <daniel.murphy.1@gmail.com>
2021-07-16 14:54:53 -0400

    added unit tests for buying and selling shares

---
+++ client/src/__tests__/buy-shares.test.js
@@ -0,0 +1,70 @@
+
+const StocksController = require('../utils/controllers/stocks-controller');
+const UserController = require('../utils/controllers/user-controller');
+
+describe('Buying Shares', () => {
+    describe('Buying a New Stock', () => {
+        test('should add new stock symbol, company name, shares owned, and shares value to DB ', async () => {
+        
+            const userId = 'testUser';
+            const symbol = 'TSTNG';
+            const companyName = 'TESTING';
+            const sharesToBuy = 10;
+            const sharesValue = 100;
+            
+            StocksController.buyNewStock = jest.fn();
+            StocksController.buyNewStock.mockReturnValue([userId, symbol, companyName, sharesToBuy, sharesValue]);
+
+            const result = await StocksController.buyNewStock(userId, symbol, companyName, sharesToBuy, sharesValue);
+            expect(result).toEqual([userId, symbol, companyName, sharesToBuy, sharesValue]);
+
+        });
+
+        test('should subtract shares value from user wallet balance', async () => {
+            
+            const oldBalance = 1000;
+            const sharesValue = 100;
+            const id = '100';
+            const newBalance = oldBalance - sharesValue;
+
+            UserController.trade = jest.fn();
+            UserController.trade.mockReturnValue([newBalance, id]);
+
+            const result = UserController.trade(newBalance, id);
+            expect(result).toEqual([newBalance, id]);
+        });
+    });
+
+    describe('Buying more shares of a stock already owned', () => {
+        test('should add the new shares to the current and update the new total shares value to the DB for the correct stock', async () => {
+
+            const userId = 'testUser';
+            const oldShares = 15;
+            const purchaseShares = 10;
+            const newShares = oldShares + purchaseShares;
+            const symbol = 'TSTNG';
+            const oldSharesValue = 100;
+            const newValue = newShares * oldSharesValue;
+
+            StocksController.buyShares = jest.fn();
+            StocksController.buyShares.mockReturnValue([newShares, symbol, userId, newValue]);
+
+            const result = StocksController.buyShares(newShares, symbol, userId, newValue);
+            expect(result).toEqual([newShares, symbol, userId, newValue])
+        });
+
+        test('should subtract shares value from user wallet balance', async () => {
+            
+            const oldBalance = 1000;
+            const sharesValue = 100;
+            const id = '100';
+            const newBalance = oldBalance - sharesValue;
+
+            UserController.trade = jest.fn();
+            UserController.trade.mockReturnValue([newBalance, id]);
+
+            const result = UserController.trade(newBalance, id);
+            expect(result).toEqual([newBalance, id]);
+        });
+    });
+});
---
+++ client/src/__tests__/sell-shares.test.js
@@ -0,0 +1,70 @@
+const StocksController = require('../utils/controllers/stocks-controller');
+const UserController = require('../utils/controllers/user-controller');
+
+describe('Selling Shares', () => {
+    describe('Selling less than all shares of a stock', () => {
+        test('should subtract the sold shares from the current and update the new total shares value to the DB for the correct stock', async () => {
+            
+            const userId = 'testUser';
+            const symbol = 'TSTNG';
+            const oldShares = 15;
+            const sharesToSell = 10;
+            const newShares = oldShares - sharesToSell;
+            const oldSharesValue = 100;
+            const newValue = newShares * oldSharesValue;
+
+
+            StocksController.sellShares = jest.fn();
+            StocksController.sellShares.mockReturnValue([newShares, userId, symbol, newValue])
+
+            const result = await StocksController.sellShares(newShares, userId, symbol, newValue);
+            expect(result).toEqual([newShares, userId, symbol, newValue]);
+        });
+
+        test('should add shares value to user wallet balance', async () => {
+
+            const oldBalance = 1000;
+            const sharesValue = 100;
+            const id = '100';
+            const newBalance = oldBalance + sharesValue;
+
+            UserController.trade = jest.fn();
+            UserController.trade.mockReturnValue([newBalance, id]);
+
+            const result = UserController.trade(newBalance, id);
+            expect(result).toEqual([newBalance, id]);
+        });
+    });
+
+    describe('Selling all shares of a stock', () => {
+        test('should delete the record of the stock from the DB', async () => {
+            const userId = 'testUser';
+            const symbol = 'TSTNG';
+            const oldShares = 15;
+            const sharesToSell = 15;
+            const newShares = oldShares - sharesToSell;
+            const oldSharesValue = 100;
+            const newValue = newShares * oldSharesValue;
+
+
+            StocksController.sellShares = jest.fn();
+            StocksController.sellShares.mockReturnValue([newShares, userId, symbol])
+
+            const result = await StocksController.sellShares(newShares, userId, symbol);
+            expect(result).toEqual([newShares, userId, symbol]);
+        });
+
+        test('should add shares value to user wallet balance', async () => {
+            const oldBalance = 1000;
+            const sharesValue = 100;
+            const id = '100';
+            const newBalance = oldBalance + sharesValue;
+
+            UserController.trade = jest.fn();
+            UserController.trade.mockReturnValue([newBalance, id]);
+
+            const result = UserController.trade(newBalance, id);
+            expect(result).toEqual([newBalance, id]);
+        });
+    });
+});

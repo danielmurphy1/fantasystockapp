57743ec6916907dc9cb7efdba95ea1f5d45402b7
Dan <daniel.murphy.1@gmail.com>
2021-07-19 18:34:08 -0400

    moved middleware folder to root directory - works

--- routes/stocks-routes.js
+++ routes/stocks-routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const StocksController = require('../controllers/stocks-controller');
-const isAuth = require('../client/src/utils/middleware/authMiddleware');
+const isAuth = require('../middleware/authMiddleware');
 
 const router = express.Router();
 

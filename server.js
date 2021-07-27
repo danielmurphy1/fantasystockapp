3d37d0412e8b3bd12d2f19b8dae05c9be4c60c15
Dan <daniel.murphy.1@gmail.com>
2021-07-26 21:57:31 -0400

    removed the client connection from server.js

--- server.js
+++ server.js
@@ -29,33 +29,33 @@ pool.connect({
     user: process.env.DB_USER, 
     password: process.env.DB_PASSWORD
     })
-    .then(() => {
-        app.listen(port, () => {
-            console.log(`Listening on port: ${port}.`);
-        });
-    })
-    .catch(err => {
-        console.error(err)
-});
+//     .then(() => {
+//         app.listen(port, () => {
+//             console.log(`Listening on port: ${port}.`);
+//         });
+//     })
+//     .catch(err => {
+//         console.error(err)
+// });
 
-const { Client } = require('pg');
+// const { Client } = require('pg');
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false
+//   }
+// });
 
-client.connect();
+// client.connect();
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
+// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//   if (err) throw err;
+//   for (let row of res.rows) {
+//     console.log(JSON.stringify(row));
+//   }
+//   client.end();
+// });
 
 app.listen(port, () =>{
   console.log(`listening on port ${port}`);

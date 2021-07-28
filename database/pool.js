32d4ca37a5ad4fad67453070e80c77f72bfb17ef
Dan <daniel.murphy.1@gmail.com>
2021-07-27 23:45:32 -0400

    sever and pool

--- database/pool.js
+++ database/pool.js
@@ -30,6 +30,7 @@ const pool = new Pool({
 
 module.exports ={
     query: (text, params, callback) => {
-        return pool.query(text, params, callback)
+        return pool.query(text, params, callback), 
+    pool
     }
 };

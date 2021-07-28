f627232182440a0f8682ee84e055c333583b6a4f
Dan <daniel.murphy.1@gmail.com>
2021-07-27 20:21:11 -0400

    deleted disconnect script from client server in server.js

--- server.js
+++ server.js
@@ -54,7 +54,6 @@ client.query('SELECT table_schema,table_name FROM information_schema.tables;', (
   for (let row of res.rows) {
     console.log(JSON.stringify(row));
   }
-  client.end();
 });
 
 app.listen(port, () =>{

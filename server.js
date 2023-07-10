9a1e99711b2a51a82ccc7dcc693ca1c8fbf38262
danielmurphy1 <daniel.murphy.1@gmail.com>
2023-07-10 00:50:37 -0400

    deploy backend

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ app.use(stocksRouter);
 app.use(bodyParser.json());
 
 // app.use("/", express.static(path.join(__dirname, "client/build")));
-app.use("/", express.static("client/build"));
+app.use(express.static("client/build"));
 
 // app.use("/trade", express.static(path.join(__dirname, "client/build")));
-app.use("/trade", express.static("client/build"));
-// app.use("/portfolio", express.static(path.join(__dirname, "client/build")));
-app.use("/portfolio", express.static( "client/build"));
+// app.use("/trade", express.static("client/build"));
+// // app.use("/portfolio", express.static(path.join(__dirname, "client/build")));
+// app.use("/portfolio", express.static( "client/build"));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"))

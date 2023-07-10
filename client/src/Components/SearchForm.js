cb53bc3d1cb0fb164ee9af53d5356e2d396829ba
danielmurphy1 <daniel.murphy.1@gmail.com>
2023-07-10 00:38:08 -0400

    deploy backend

--- client/src/Components/SearchForm.js
+++ client/src/Components/SearchForm.js
@@ -3,7 +3,7 @@ import { Form, Button } from 'react-bootstrap';
 
 function SearchForm(props) {
     return(
-        <Form inline className="justify-content-around mb-3 d-flex" onSubmit={(event)=>{props.handleStockSearch(event, props.inputValue)}}>
+        <Form className="justify-content-around mb-3 d-flex" onSubmit={(event)=>{props.handleStockSearch(event, props.inputValue)}}>
             <Form.Group className="d-flex">
                 {/* <Form.Label htmlFor="stockSymbolSearch" srOnly>Stock Symbol</Form.Label> */}
                 <Form.Control type="text" id="stockSymbolSearch" placeholder="Enter Stock Symbol" className="mr-1" value={props.inputValue} onChange={props.handleInputValueChange} required/>
